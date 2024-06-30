65d5674e327de13d3c44808041cf19b23af4f2a4
DustinYansberg <DustinYansberg@gmail.com>
2024-06-30 15:19:05 -0700

    wired up get all API call for Offices and made basic display

--- pmt-angular/src/app/offices/offices.component.ts
+++ pmt-angular/src/app/offices/offices.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-offices',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './offices.component.html',
-  styleUrl: './offices.component.css'
+  styleUrl: './offices.component.css',
 })
 export class OfficesComponent {
+  data: any = [];
 
+  constructor(private http: HttpClient) {
+    http
+      .get('http://localhost:8080/office', { observe: 'response' })
+      .subscribe((response) => {
+        console.log(response.body);
+        this.data = response.body;
+      });
+  }
 }
