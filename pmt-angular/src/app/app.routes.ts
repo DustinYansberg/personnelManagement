905debea8f3336c9fbd737cd00f2332bb21f0ae9
DustinYansberg <DustinYansberg@gmail.com>
2024-06-30 14:55:05 -0700

    set up routing for employees, home, offices

--- pmt-angular/src/app/app.routes.ts
+++ pmt-angular/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OfficesComponent } from './offices/offices.component';
+import { EmployeesComponent } from './employees/employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'offices',
+    component: OfficesComponent,
+  },
+  {
+    path: 'employees',
+    component: EmployeesComponent,
+  },
+];
+
+// {
+//     path:'',
+//     component:
+// },
