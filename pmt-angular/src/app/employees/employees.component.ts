9c1d93b7c49bb834105d09bce7092cb81e3d67bd
DustinYansberg <DustinYansberg@gmail.com>
2024-06-30 15:18:42 -0700

    wired up get all API call for employees and made basic display

--- pmt-angular/src/app/employees/employees.component.ts
+++ pmt-angular/src/app/employees/employees.component.ts
@@ -1,12 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css',
 })
 export class EmployeesComponent {
   data: any = [];
+
+  constructor(private http: HttpClient) {
+    http
+      .get('http://localhost:8080/employee', { observe: 'response' })
+      .subscribe((response) => {
+        console.log(response.body);
+        this.data = response.body;
+      });
+  }
 }
