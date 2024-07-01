230e2e585ee0c8c02c6bc206ecac679321aa0dac
DustinYansberg <DustinYansberg@gmail.com>
2024-06-30 19:35:24 -0700

    added office post method and table sorting

--- pmt-angular/src/app/offices/offices.component.ts
+++ pmt-angular/src/app/offices/offices.component.ts
@@ -1,23 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-offices',
   standalone: true,
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './offices.component.html',
   styleUrl: './offices.component.css',
 })
 export class OfficesComponent {
   data: any = [];
+  newOfficeName: string = '';
 
   constructor(private http: HttpClient) {
     http
       .get('http://localhost:8080/office', { observe: 'response' })
       .subscribe((response) => {
-        console.log(response.body);
         this.data = response.body;
+        this.data.sort((a: any, b: any) => a.officeId - b.officeId);
+      });
+  }
+
+  addOffice() {
+    this.http
+      .post(
+        'http://localhost:8080/office',
+        { name: this.newOfficeName },
+        { observe: 'response' }
+      )
+      .subscribe((response) => {
+        this.data.push(response.body);
+        this.newOfficeName = '';
       });
   }
 }
