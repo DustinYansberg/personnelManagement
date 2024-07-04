5d80b66d38c422858f3ec9ab62530e4e23898432
DustinYansberg <DustinYansberg@gmail.com>
2024-07-04 11:00:23 -0700

    finishing touches on add and delete employee functionality

--- src/main/java/com/pmt/controllers/EmployeeController.java
+++ src/main/java/com/pmt/controllers/EmployeeController.java
@@ -3,9 +3,11 @@ package com.pmt.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,12 +42,14 @@ public class EmployeeController {
     }
 
     // update by id
-    public ResponseEntity<Employee> updateById(int id, Employee employee) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateById(@PathVariable int id, Employee employee) {
 	return service.updateEmployee(id, employee);
     }
 
     // delete by id
-    public ResponseEntity<Employee> deleteById(int id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Employee> deleteById(@PathVariable int id) {
 	return service.deleteEmployee(id);
     }
 
